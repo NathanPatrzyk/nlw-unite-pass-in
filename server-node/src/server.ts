d1dc052d67ba93b75bad4e5d3d55849e98657ef0
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-04-03 18:25:43 -0300

    v2.0

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -2,16 +2,8 @@ import fastify from "fastify"
 
 const app = fastify()
 
-app.get('/', () => {
+app.post('/events', (request, reply) => {
     return 'Hello NLW Unite'
-})
-
-app.get('/users', () => {
-    return 'Hello Teste'
-})
-
-app.post('/users', () => {
-    return 'Hello Teste'
 })
 
 app.listen({port: 3333}).then(() => {
